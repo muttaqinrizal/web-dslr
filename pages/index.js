15844e6df33e9fcd319b17b9b81cf4607645243a
muttaqinrizal <rizalm57@gmail.com>
2024-02-13 15:15:05 +0700

    init

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { Camera } from "web-gphoto2";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const camera = new Camera();
+
+  const connectCamera = async () => {
+    await Camera.showPicker()
+  }
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
@@ -13,6 +20,7 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className="font-mono font-bold">pages/index.js</code>
         </p>
+        <button onClick={() => connectCamera()}>Pilih Kamera</button>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
