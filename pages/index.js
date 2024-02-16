f6509a547acc8c76942de318865585fc8d901993
muttaqinrizal <rizalm57@gmail.com>
2024-02-16 14:00:35 +0700

    set camera state

--- pages/index.js
+++ pages/index.js
@@ -41,7 +41,7 @@ export default function Home() {
     if (connected) {
       tryToConnectToCamera()
     }
-  }, [connected])
+  }, [])
 
   const tryToConnectToCamera = async () => {
     let camera;
