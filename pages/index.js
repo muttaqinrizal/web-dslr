6e952612aa59acba8af50cff7960f8a6504a7561
Masruh Choeroni <m.choerni1988@gmail.com>
2024-02-16 11:10:49 +0700

    preview horizontal image list

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,39 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import { Camera } from "web-gphoto2";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const [images, setImages] = useState(null)
+  const [images, setImages] = useState([])
+  const [previewImageUrl, setPreviewImageUrl] = useState(null);
   const camera = new Camera();
 
   const connectCamera = async () => {
     await Camera.showPicker()
     await camera.connect();
+    this.setState({ type: 'Status', message: '⌛ Connecting...' });
+    console.log(await camera.connect());
   }
 
   const captureCamera = async () => {
     const file = await camera.captureImageAsFile()
     const imageUrl = URL.createObjectURL(file);
-    let a = document.createElement('a')
-    a.download = 'images.jpeg'
-    a.href = imageUrl
-    a.click();
+    // let a = document.createElement('a')
+    // a.download = 'images.jpeg'
+    // a.href = imageUrl
+    // a.click();
 
-    console.log('full response imageURL', file);
-    console.log('imageURL', imageUrl);
-    setImages(imageUrl)
-    
+    // console.log('full response imageURL', file);
+    // console.log('imageURL', imageUrl);
+    // setImages(imageUrl)
+    // setPreviewImageUrl(imageUrl);
+    // setImages(imageUrl);
+
+    // Update the images array with the new image URL
+    setImages([...images, imageUrl]);
+    // setPreviewImageUrl(imageUrl);
   }
 
   const getCameraConfig = async() => {
@@ -38,17 +46,32 @@ export default function Home() {
     console.log("Supported Ops:", ops);
   }
 
-  const capturePreviewAsBlob = async() => {
+  const capturePreviewAsBlob = async () => {
     // Capture a frame while in live view mode
     const blob = await camera.capturePreviewAsBlob();
     const imageUrl = URL.createObjectURL(blob);
-    console.log('preview blob', imageUrl)
-    // Set the imageUrl as the src of an image element in your HTML
+
+    setImages([...images, imageUrl]);
+    // setPreviewImageUrl(imageUrl);
   }
+
+  // const capturePreviewAsBlob = async() => {
+  //   // Capture a frame while in live view mode
+  //   const blob = await camera.capturePreviewAsBlob();
+  //   const imageUrl = URL.createObjectURL(blob);
+  //   console.log('preview blob', imageUrl)
+  //   // Set the imageUrl as the src of an image element in your HTML
+  // }
+
+  // useEffect to automatically connect the camera on component mount
+  useEffect(() => {
+    // connectCamera();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []); // empty dependency array ensures it runs only once on mount
   
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center p-24 ${inter.className}`}
     >
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <button onClick={() => connectCamera()}>Pilih Kamera</button>
@@ -56,6 +79,19 @@ export default function Home() {
         <button onClick={() => capturePreviewAsBlob()}>Capture Kamera as Blob</button>
         <button onClick={() => getCameraConfig()}>Kamera Konfig</button>
         <button onClick={() => getSupportedOps()}>Kamera Support</button>
+      </div>
+
+      <div>
+        <h2 className="w-full text-center p-10">Captured Images:</h2>
+        <div className="grid grid-cols-4 gap-4">
+          {images.length > 0 && (
+            images.map((imgUrl, index) => (
+              <div key={index}>
+                <Image src={imgUrl} alt={`Captured Image ${index + 1}`} width={300} />
+              </div>
+            ))
+          )}
+        </div>
       </div>
     </main>
   );
